7cb442137d7cc221679fcf35de9ca4f84146daad
eden.kim <eden.kim@goorm.io>
2023-07-11 09:13:49 +0000

    gradle bin added

--- src/main/java/com/example/kakao/KakaoApplication.java
+++ src/main/java/com/example/kakao/KakaoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KakaoApplication {
 
 	public static void main(String[] args) {
+        ArrayList<Integer> = ssasdfwsdadf
 		SpringApplication.run(KakaoApplication.class, args);
 	}
 
